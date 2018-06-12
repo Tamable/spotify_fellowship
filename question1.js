07cd16df899bc811c16c985f710f01abca519823
Tamae <t728532417114m@gmail.com>
2018-06-12 17:12:44 -0400

    question 2

--- question1.js
+++ question1.js
@@ -1,15 +1,13 @@
 // Question 1 -- sortByStrings(s,t): Sort the letters in the string s by the order they occur in the string t. You can assume t will not have repetitive characters. For s = "weather" and t = "therapyw", the output should be sortByString(s, t) = "theeraw". For s = "good" and t = "odg", the output should be sortByString(s, t) = "oodg".
 
 const sortByStrings = (s, t) => {
-  sArr = s.split('');
-  chWithIdx = []
+  let sArr = s.split('');
+  let chWithIdx = [];
   sArr.forEach((ch) => {
-    chWithIdx.push([t.indexOf(ch), ch])
-  })
+    chWithIdx.push([t.indexOf(ch), ch]);
+  });
 
   return chWithIdx.sort().map((pair) => {
-    return pair[1]
-  }).join('')
+    return pair[1];
+  }).join('');
 }
-
-console.log(sortByStrings('good', 'odg'))
---
+++ question2.js
@@ -0,0 +1,23 @@
+// Question 2 -- decodeString(s): Given an encoded string, return its corresponding decoded string.
+//
+// The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is repeated exactly k times. Note: k is guaranteed to be a positive integer.
+//
+// For s = "4[ab]", the output should be decodeString(s) = "abababab"
+// For s = "2[b3[a]]", the output should be decodeString(s) = "baaabaaa"
+
+const decodeString = (s) => {
+  let decodedStr = '';
+  let repeatNum;
+  for (let i = 0; i < s.length; i++) {
+    let numCheck = Number(s[i]);
+    if (isNaN(numCheck) === false) {
+      repeatNum = numCheck;
+    } else if (s[i] === '[') {
+      decodedStr += (decodeString(s.slice(i + 1))).repeat(repeatNum - 1);
+    } else if (s[i] === ']') {
+      return decodedStr;
+    } else {
+      decodedStr += s[i];
+    }
+  }
+}
