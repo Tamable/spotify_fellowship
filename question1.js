3cec96c9ed5b718d58c89aa9ac8e03bc8bb7e2f7
Tamae <t728532417114m@gmail.com>
2018-06-12 13:46:47 -0400

    question1

---
+++ question1.js
@@ -0,0 +1,15 @@
+// Question 1 -- sortByStrings(s,t): Sort the letters in the string s by the order they occur in the string t. You can assume t will not have repetitive characters. For s = "weather" and t = "therapyw", the output should be sortByString(s, t) = "theeraw". For s = "good" and t = "odg", the output should be sortByString(s, t) = "oodg".
+
+const sortByStrings = (s, t) => {
+  sArr = s.split('');
+  chWithIdx = []
+  sArr.forEach((ch) => {
+    chWithIdx.push([t.indexOf(ch), ch])
+  })
+
+  return chWithIdx.sort().map((pair) => {
+    return pair[1]
+  }).join('')
+}
+
+console.log(sortByStrings('good', 'odg'))
